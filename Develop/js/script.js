77adda9aac77b8872b1736cffc840ac374ec2840
DJMIKE20 <m.zimm20@gmail.com>
2022-08-11 14:42:46 -0400

    added current-timer and initial schedule view and save button

---
+++ Develop/js/script.js
@@ -0,0 +1,17 @@
+
+// Global Variables 
+
+
+
+// current time in the Jumbotron
+setInterval (function() {
+    document.getElementById('currentDay').innerHTML =  moment().format('MMMM Do YYYY, h:mm:ss a');
+}, 1000);
+  
+
+
+
+
+
+// update textarea based off the past, present, future
+
