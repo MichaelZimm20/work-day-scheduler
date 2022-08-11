11f2715b21b5064b365cbe054111e3140fdffc4c
DJMIKE20 <m.zimm20@gmail.com>
2022-08-11 15:04:22 -0400

    updated script.js to read from localStorage and loop over all

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -9,6 +9,39 @@ setInterval (function() {
 }, 1000);
   
 
+// textarea elements 
+var time9am = $('#9AM');
+var time10am = $('#10AM');
+var time11am = $('#11AM');
+var time12pm = $('#12PM');
+var time1pm = $('#1PM');
+var time2pm = $('#2PM');
+var time3pm = $('#3PM');
+var time4pm = $('#4PM');
+var time5pm = $('#5PM');
+
+// text area elements in array 
+var timeArray = [
+    time9am,
+    time10am,
+    time11am,
+    time12pm,
+    time1pm,
+    time2pm,
+    time3pm,
+    time4pm,
+    time5pm
+];
+
+// check localStorage, get from localStorage
+function loadScheduler() {
+    for (let el of timeArray) {
+        el.val(JSON.parse(localStorage.getItem(el.data("hour"))));
+
+    }
+};
+
+// save buttons
 
 
 
