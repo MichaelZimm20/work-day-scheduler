8511189cdabc749aacab06c1f91aae61c1b9ab44
DJMIKE20 <m.zimm20@gmail.com>
2022-08-16 10:51:14 -0400

    refactored code and added readme

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,53 +1,85 @@
 
 // current time in the Jumbotron
-setInterval (function() {
-    document.getElementById('currentDay').innerHTML =  moment().format('MMMM Do YYYY, h:mm:ss a');
+setInterval(function () {
+    document.getElementById('currentDay').innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
 }, 1000);
-  
 
-// textarea elements 
-var time9am = $('#9AM');
-var time10am = $('#10AM');
-var time11am = $('#11AM');
-var time12pm = $('#12PM');
-var time1pm = $('#1PM');
-var time2pm = $('#2PM');
-var time3pm = $('#3PM');
-var time4pm = $('#4PM');
-var time5pm = $('#5PM');
 
-// text area elements in array 
-var timeArray = [
-    time9am,
-    time10am,
-    time11am,
-    time12pm,
-    time1pm,
-    time2pm,
-    time3pm,
-    time4pm,
-    time5pm
-];
-
-loadScheduler();
-
-// check localStorage, get from localStorage
-function loadScheduler() {
-    for (let el of timeArray) {
-        el.val(localStorage.getItem(el.data("hour")));
-        // el.val(JSON.parse(localStorage.getItem(el.data("hour"))));
-
-    }
-};
 
 
 // save buttons and save to localStorage 
-$('.saveBtn').on('click', function(event){
-   var saveClick = $(this).siblings('textarea');
-   var speciicTime = saveClick.data('hour');
-    JSON.stringify(localStorage.setItem(speciicTime, saveClick.val()));
-   
+$('.saveBtn').on('click', function () {
+    var saveClick = $(this).siblings('.description').val();
+    var time = $(this).parent().attr("id");
+
+    // set item to localStorage
+    localStorage.setItem(time, saveClick);
+
+    // timeout interval to show saved message notification
+    $(".notification").addClass("show");
+    setTimeout(function () {
+        $(".notification").removeClass("show");
+    }, 2000);
+
 });
+
+
+
+// update textarea based off the past, present, future
+function backgroundUpdate() {
+    var currentHour = moment().hours();
+
+    // looop through each time block and color highlight specific time block
+    $(".time-block").each(function () {
+        var hourBlock = parseInt($(this).attr("id").split("-")[1]);
+
+        if (hourBlock < currentHour) {
+            $(this).addClass("past");
+        } else if (hourBlock === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+//call backgroundUpdate function
+backgroundUpdate();
+
+
+// Interval to check for every 15 secs for new messages to scheduler
+var interval = setInterval(backgroundUpdate, 15000);
+
+// get items from localStorage
+function retrieveItems() {
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-18"));
+
+}
+
+
+// call getitems function
+retrieveItems();
+
+
+
+
+// clear my schedule button
+$('#clear-schedule').on('click', function () {
+    localStorage.clear();
+    retrieveItems();
+});
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # Work Day Scheduler Starter Code
+
+
+## User Story 
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+
+## Acceptance Criteria 
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+
+## Consist Of 
+```
+* Bootstrap, jQuery, Moment.js, JavaScript, CSS, HTML
+
+* Loops, if statements 
+
+* Callbacks, event Listeners
+
+* return statements 
+
+* Split methods 
+
+* Semantics and logical statements 
+
+* setIntervals and setTimeouts
+
+```
+
+## Important Links 
+
+* GitHub Link:
+
+* Deployable Live Link: 
+
+
+## Screenshots
+
+![Work Scheduler screenshots](./Develop/images/initial-page-view.png "Initial page View") 
+<br/><br/><br/><br/><br/>
+
+![Work Scheduler screenshots](./Develop/images/message-saved.png "Message Saved")
+<br/><br/><br/><br/>
+
+![Work Scheduler screenshots](./Develop/images/clear-schedule-button.png "Clear Schedule Button") 
+<br/><br/><br/><br/>
+
+![Work Scheduler screenshots](./Develop/images/clear-button-activated.png "Clear Button Activated") 
+<br/><br/><br/><br/>
+
+![Work Scheduler screenshots](./Develop/images/localStorage.png "localStorage") 
+<br/><br/><br/><br/>
